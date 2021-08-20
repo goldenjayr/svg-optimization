9b2e2a582c72c13a471b139700fe64f9826765f8
goldenjayr <jayrjoseph@dnamicro.com>
2021-08-20 09:57:17 +0800

    updated

--- moveAllFillToStyle.js
+++ moveAllFillToStyle.js
@@ -86,6 +86,56 @@ exports.fn = function (item) {
 			node.attributes['class'] = className
       delete node.attributes['fill']
     }
+
+		if (node.hasAttr('style')) {
+			const style = node.attributes['style']
+			if (style.includes('fill')) {
+				const regex = /(?<=fill:).*/
+				const matches = style.match(regex)
+				if (matches) {
+					const fill = matches[0]
+					if (fill === 'none') {
+						delete node.attributes['style']
+						return
+					}
+					if (!styleElement.children.length) {
+						const elem = createContentItem({ type: 'text', value: `.st0{ fill: ${fill} }` })
+						styleElement.spliceContent(0, 0, elem)
+						delete node.attributes['style']
+						node.addAttr('class')
+						node.attributes['class'] = `st0`
+						return
+					}
+
+					let clasName = ''
+					const hasFill = styleElement.children.some((item) => {
+						const color = new RegExp(fill)
+						const matches = item && color.test(item.value)
+						if (matches) {
+							className = getClassName(item.value)
+						}
+						return matches
+					})
+					if (!hasFill) {
+						const lastElement = styleElement.children[styleElement.children.length - 1]
+						const selector = getClassName(lastElement.value)
+						const num = Number(selector.replace(/\D/g, '')) + 1
+
+						const elem = createContentItem({ type: 'text', value: `.st${num}{ fill: ${fill} }` })
+						styleElement.spliceContent(styleElement.children.length, 0, elem)
+						delete node.attributes['style']
+						node.addAttr('class')
+						node.attributes['class'] = `st${num}`
+						return
+					}
+
+					node.addAttr('class')
+					node.attributes['class'] = className
+					delete node.attributes['style']
+				}
+			}
+
+		}
   }
 
   return {
